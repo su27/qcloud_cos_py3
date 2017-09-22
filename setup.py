2bc24aeca82a97fbf771b647c28cb559be8518f8
sudan <sudan@douban.com>
2017-09-23 00:05:02 +0800

    update requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires = ['aiohttp']
+install_requires = ['aiohttp', 'requests']
 dependency_links = []
 
 setup(
